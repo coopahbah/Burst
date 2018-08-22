6ef76637106e3856c72980ff355c84a07d78a945
Cooper Barth <coop10101@gmail.com>
2018-08-22 16:59:21 -0500

    dot generation at intervals

--- Burst/GameScene.swift
+++ Burst/GameScene.swift
@@ -3,19 +3,23 @@ import GameplayKit
 import Darwin
 
 class GameScene: SKScene {
-    private let screenSize = UIScreen.main.bounds
+    private let screenSize: CGRect = UIScreen.main.bounds
+    private var maxX: Double = 0.0
+    private var minX: Double = 0.0
+    private var maxY: Double = 0.0
+    private var minY: Double = 0.0
+    private var bubbleTimer: Timer! = Timer()
     
     override func didMove(to view: SKView) {
-        createBubble()
+        maxX = Double(screenSize.width - 10.0)
+        minX = -1 * maxX
+        maxY = Double(screenSize.height - 10.0)
+        minY = -1 * maxY
+        
+        bubbleTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(createBubble), userInfo: nil, repeats: true)
     }
     
-    func createBubble() {
-        let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
-        let maxX = Double(screenWidth - 10.0)
-        let minX = -1 * maxX
-        let maxY = Double(screenHeight - 10.0)
-        let minY = -1 * maxY
+    @objc func createBubble() {
         let xPosition = Double.random(in: minX...maxX)
         let yPosition = Double.random(in: minY...maxY)
         let bubble = SKShapeNode.init(circleOfRadius: 10.0)
